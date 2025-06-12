65c31dd1718395d603b6bded6558ae39e09a0437
Sayantani630 <2305242@kiit.ac.in>
2025-06-12 15:27:58 +0530

    Update mdps_publics.py

--- mdps_publics.py
+++ mdps_publics.py
@@ -1,24 +1,18 @@
 import pickle
-... import streamlit as st
-... from streamlit_option_menu import option_menu
-... 
-... # loading the saved models
-... 
-... diabetes_model = pickle.load(open('c:\\users\\kiit0001\\desktop\\multiple disease prediction\\diabetes_model.sav','rb'))
-... 
-... heart_disease_model = pickle.load(open('c:\\users\\kiit0001\\desktop\\multiple disease prediction\\heart_disease_model.sav','rb'))
-... 
-... parkinsons_model = pickle.load(open('c:\\users\\kiit0001\\desktop\\multiple disease prediction\\parkinsons_model.sav','rb'))
-... 
-... 
-... 
-... # sidebar for navigation
-... with st.sidebar:
-...     
-...     selected = option_menu('Multiple Disease Prediction System',
-...                           
-...                           ['Diabetes Prediction',
-...                            'Heart Disease Prediction',
+import streamlit as st
+from streamlit_option_menu import option_menu 
+# loading the saved models
+diabetes_model = pickle.load(open('c:\\users\\kiit0001\\desktop\\multiple disease prediction\\diabetes_model.sav','rb'))
+
+heart_disease_model = pickle.load(open('c:\\users\\kiit0001\\desktop\\multiple disease prediction\\heart_disease_model.sav','rb'))
+parkinsons_model = pickle.load(open('c:\\users\\kiit0001\\desktop\\multiple disease prediction\\parkinsons_model.sav','rb'))
+# sidebar for navigation
+with st.sidebar:
+     
+ selected = option_menu('Multiple Disease Prediction System',
+                         
+                         ['Diabetes Prediction',
+                           'Heart Disease Prediction',
 ...                            'Parkinsons Prediction'],
 ...                           icons=['activity','heart','person'],
 ...                           default_index=0)
