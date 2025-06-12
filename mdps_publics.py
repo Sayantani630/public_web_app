a4ba0fcbf520b058563082c6aaa8fc0652b0cc8d
Sayantani630 <2305242@kiit.ac.in>
2025-06-12 15:38:35 +0530

    Update mdps_publics.py

--- mdps_publics.py
+++ mdps_publics.py
@@ -1,6 +1,6 @@
 import pickle
 import streamlit as st
-from streamlit_option_menu import option_menu 
+ 
 # loading the saved models
 diabetes_model = pickle.load(open('c:\\users\\kiit0001\\desktop\\multiple disease prediction\\diabetes_model.sav','rb'))
 
